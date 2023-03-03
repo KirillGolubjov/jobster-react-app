84e99e20adbd2efe4863beabfc89e17b6a8e6587
Kirill Golubjov <kirillcnz@gmail.com>
2023-03-03 15:32:24 +0200

    Small Sidebar - Nav links

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -4,6 +4,7 @@ import { FaTimes } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleSidebar } from '../features/user/userSlice';
+import links from '../utils/links';
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user);
@@ -25,7 +26,23 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className='nav-links'>nav links</div>
+          <div className='nav-links'>
+            {links.map((link, index) => {
+              const { id, text, path, icon } = link;
+              return (
+                <NavLink
+                  key={id}
+                  to={path}
+                  className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                  }
+                >
+                  <span className='icon'>{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Wrapper>
